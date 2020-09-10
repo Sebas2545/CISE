676c5ce0ce9fa22a036267570a6ba884654d1eb9
Sebas2545 <50222638+Sebas2545@users.noreply.github.com>
2020-09-10 16:51:55 -0700

    Add files via upload

--- HW3/CISE_HW3_ANNOTATED.cpp
+++ HW3/CISE_HW3_ANNOTATED.cpp
@@ -45,12 +45,12 @@ General Notes
 - If all elements of a2 appear in a1 in order (not necessarily consecutively), return true
 	- Yes, every set contains the empty set (n2 = 0)
 
-10. int meld(const string a1[], int n1, const string a2[], int n2, string result[], int max); // ASK IF ARRAYS MUST BE STRICTLY NON-DECREASING
+10. int meld(const string a1[], int n1, const string a2[], int n2, string result[], int max);
 - if both a1 & a2 have elements in nondecreasing order, place the elements of a1 & a2 in result
 such that result is also nondecreasing and return the number of elements placed
 - if the result would overflow result OR a1 and/or a2 are not in nondecreasing order, return -1
 
-11. int split(string a[], int n, string splitter);
+11. int split(string a[], int n, string splitter); DONE/NOT TESTED
 - Reorder the array such that the front is less than splitter, and the back is greater than splitter
 	- Not necesarrily in decreasing order, each side may be unordered so long as each side is distinct
 - Return the position of the first element that is NOT < splitter (first element of back half, return n if no such element exists)
@@ -74,7 +74,16 @@ int moveToBack(string a[], int n, int pos);
 
 
 
+
+
+int meld(const string a1[], int n1, const string a2[], int n2, string result[], int max);
+
+int split(string a[], int n, string splitter);
+
+
 bool helper_check_negative(int n);
+
+bool helper_check_nondecreasing(int* arr, int n); 
 
 //START MAIN/TESTING
 int main() {
@@ -156,8 +165,80 @@ int moveToBack(string a[], int n, int pos) { // Move element @ pos to the back a
 
 
 
+
+int meld(const string a1[], int n1, const string a2[], int n2, string result[], int max) { // if both a1 & a2 have elements in nondecreasing order, place the elements of a1 & a2 in result
+    if(helper_check_negative(n1) || n1 == 0) // size error checking                        // such that result is also nondecreasing and return the number of elements placed
+		return -1;
+	if(helper_check_negative(n2) || n2 == 0)
+		return -1;
+	if(helper_check_negative(max) || max == 0 || n1 + n2 > max)
+		return -1;
+	
+	// check that both arrays are nondecreasing
+	if(helper_nondecreasing(a1, n1))
+		return -1;
+	if(helper_nondecreasing(a2, n2))
+		return -1;
+	
+	int iterator_a1 = 0;
+	int iterator_a2 = 0;
+	
+	for(int i = 0; i < n1 + n2; ++i) {
+		if(a1[iterator_a1] < a2[iterator_a2]) { // if a1 < a2
+			result[i] = a1[iterator_a1];
+			
+		}
+		
+		
+		
+	}
+	
+}
+
+/*
+Ideas for Algorithm
+- Sort the array in descending order, linear search from the back to find value to be returned
+	- Pros: Easy to implement, just grab a sort algorithm; Doesn't require a new array
+	- Cons: Not super efficient, as it does more work than needed
+	
+- Modified Insertion Sort: iterate through the array. 
+When an element > splitter is found, swap it with the furthest back value (that is < splitter) and iterate an iterator from the back
+	- Pros: Much more efficient, (ideally) only one pass through the array, does minimal work; Doesn't require a new array
+	- Cons: Harder to write; Doesn't cover ==splitter case well, biggest issue w/ this method
+*/
+int split(string a[], int n, string splitter) { // Reorder the array such that the front is less than splitter, and the back is greater than splitter
+	if(helper_check_negative(n) || n == 0) // if n is negative or 0 (to prevent undefined behaviour)
+		return -1; // return -1 (bad argument)
+
+	// Insertion Sort
+	for(int i = 0; i < n; ++i) {
+		for(int j = i + 1; j < n; ++j) {
+			if(a[i] > a[j]) {
+				string temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+
+	// Find index of first element > splitter
+	for(int i = 0; i < n; ++i)
+		if(a[i] > splitter)
+			return i;
+	
+	return n; // if no element > splitter is found
+}
+
 bool helper_check_negative(int n) { // checks if n is negative for bad size argument case
 	if(n < 0)
 		return true;
 	return false;
+}
+bool helper_check_nondecreasing(int* arr, int n) { // returns true if an array is nondecreasing
+
+	
+	for(int i = 0; i < n; ++i)
+	
+	
+	return true;
 }
